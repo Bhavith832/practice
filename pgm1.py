d8c562431844b186f900b6e0133f7797c9346299
Bhavith832 <bhavithnhce07@gmail.com>
2025-06-05 17:24:45 +0530

    Update pgm1.py

--- pgm1.py
+++ pgm1.py
@@ -1,3 +1,9 @@
-min = int(input("Enter the time in minutes:"))
-sec = min*60
-print("minutes in seconds:",sec)
+student_score = int(input("Enter the student score:"))
+if student_score >= 90 & student_score < 89:
+    print("The student grade is 'B'.")
+elif student_score >= 70 & student_score < 79:
+    print("The student grade is 'C'.")
+elif student_score >= 60 & student_score < 69:
+    print("The student grade is 'D'.")
+elif student_score < 60:
+    print("The student grade is 'F'.")
